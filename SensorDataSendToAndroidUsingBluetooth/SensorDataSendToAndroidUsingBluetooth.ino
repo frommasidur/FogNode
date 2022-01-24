2f46c190693c5da86a74ca11d8d867cc5101a6d4
MASIDUR RAHAMAN <frommasidur@gmail.com>
2022-01-24 23:08:58 +0530

    Adding DHT11 sensor code with Bluetooth code and work fine

--- SensorDataSendToAndroidUsingBluetooth/SensorDataSendToAndroidUsingBluetooth.ino
+++ SensorDataSendToAndroidUsingBluetooth/SensorDataSendToAndroidUsingBluetooth.ino
@@ -1,25 +1,65 @@
+#include "DHT.h"
+#define DHTPIN 4
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
+
 #include <SoftwareSerial.h>
  
-SoftwareSerial blue(2,3); // bluetooth module connected here 
- 
-int Vresistor = A0; 
-int Vdata = 0; 
+SoftwareSerial blue(2,3); // bluetooth module connected here (Rx,Tx) 
  
 void setup(){ 
-  
-   pinMode(Vresistor, INPUT); 
    Serial.begin(9600); 
+   dht.begin();
    blue.begin(9600); 
-   
 } 
 void loop(){ 
-   // read the value at analog input
-   Vdata = analogRead(Vresistor); 
- 
-blue.print("Variable Resistor: "); 
-blue.println(Vdata);
-blue.println("Electronic Clinic"); 
-blue.println("________________");
- 
-delay(1000); 
+  delay(1000);
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+  // Read temperature as Fahrenheit (isFahrenheit = true)
+  float f = dht.readTemperature(true);
+
+    // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t) || isnan(f)) {
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return;
+  }
+
+  // Compute heat index in Fahrenheit (the default)
+  float hif = dht.computeHeatIndex(f, h, true);
+  // Compute heat index in Celsius (isFahreheit = false)
+  float hic = dht.computeHeatIndex(t, h, false);
+
+  Serial.print(F("Humidity: "));
+  Serial.print(h);
+  Serial.print(F("%  Temperature: "));
+  Serial.print(t);
+  Serial.print(F("°C "));
+  Serial.print(f);
+  Serial.print(F("°F  Heat index: "));
+  Serial.print(hic);
+  Serial.print(F("°C "));
+  Serial.print(hif);
+  Serial.println(F("°F"));
+  
+  /*Send to Bluetooth*/
+  blue.print("Humidity: ");
+  blue.print(h);
+  blue.println(" %");
+  
+  blue.print("Temperature: "); 
+  blue.print(t);
+  blue.print(" °C ");
+  blue.print(f);
+  blue.println(" °F"); 
+  
+  blue.print("Heat index: ");
+  blue.print(hic); 
+  blue.print(" °C ");
+  blue.print(hif);
+  blue.println(" °F");
+  blue.println("________________");
 }
